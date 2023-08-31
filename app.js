f2c1a208b915742bf094797c204f9f12dafab5fe
massiv1984 <emil.kero@elev.ga.ntig.se>
2023-08-31 11:01:17 +0200

    ✨ variabel kod

--- app.js
+++ app.js
@@ -1,1 +1,37 @@
 console.log("hello world")
+
+let firstName;
+firstName = "Jens"
+
+let lastName = "Andreasson"
+
+let num = 23
+
+console.log(typeof(num))
+
+var NAME = "Stina"
+// NAME = "Frasse"
+console.log(NAME)
+let i;
+
+for (let i = 0; i < 10; i++) {
+    NAME = "Sture"
+    console.log(NAME)
+}
+console.log(i)
+
+var skola = "NTI"
+console.log(skola)
+
+let a = "Jens"
+let b = 'Jens'
+let c = `Mitt namn är ${a}`
+let d = 'Mitt namn är ' + b
+
+let e = 12
+let f = 16.7
+
+let g = true
+let h = false
+
+console.log(c, e, g)
